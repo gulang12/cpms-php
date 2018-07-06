21bd5ce4cb72a78192ecd4040766a94624c0cf3b
gulang <1226740471@qq.com>
2018-07-06 21:31:45 +0800

    auth info

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -29,4 +29,22 @@ class User  extends AdminBase
         return $this->fetch();
     }
 
+
+    public function addRoleAuth(){
+
+        if(request()->isPost()) {
+
+            $data = input();
+            // $submit_time = ;
+            echo microtime(); // 前部分为毫秒 后半部分为秒   用前后先加 就可以获取到当前时间精确到毫秒的时间戳 多次提交表单 时间差在1秒之内就提示
+
+            $role_id      = $data["role_id"];
+            $action       = $data['action'];
+
+            return json(['code'=>1,'msg'=>'添加成功']);
+
+        }
+       
+    }
+
 }
--- public/static/js/admin/admincommon.js
+++ public/static/js/admin/admincommon.js
@@ -1,6 +1,21 @@
-$(document).ready(function(){
+/**
+* @param len  生成字符串长度 
 
+**/
+function createRandomStr(len){
+	var len = len || 10;
+    var str = '0123456789qazwsxedcrfvtgbyhnujmiklopQAZWSXEDCRFVTGBYHUJNIKMIOPLK'; 
+    var random = "";
+    for (var i =0; i < len; i++) {
+    	
+    	random+= str[Math.ceil(Math.random()*62)];
+    }
+    
+    return random;
+} 
 
+function refreshRandomStr(el){
+	var random = createRandomStr();
 
-
-})
+	$("#"+el).val(random);
+}
