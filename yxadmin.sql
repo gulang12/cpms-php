896e8bc8b2ed1d033425f09c46976270c900ca98
lzc <1226740471@qq.com>
2018-07-08 23:05:28 +0800

    add sql

---
+++ yxadmin.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.53 : Database - yxadmin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yxadmin` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `yxadmin`;
+
+/*Table structure for table `yx_role` */
+
+DROP TABLE IF EXISTS `yx_role`;
+
+CREATE TABLE `yx_role` (
+  `role_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(30) NOT NULL COMMENT '角色名',
+  `role_auth` text NOT NULL COMMENT '角色权限',
+  `role_add` datetime NOT NULL COMMENT '角色添加时间',
+  PRIMARY KEY (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `yx_role` */
+
+/*Table structure for table `yx_user` */
+
+DROP TABLE IF EXISTS `yx_user`;
+
+CREATE TABLE `yx_user` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_login` varchar(60) NOT NULL COMMENT '用户登入名',
+  `user_pass` varchar(255) NOT NULL COMMENT '用户密码',
+  `user_nicename` varchar(50) NOT NULL COMMENT '用户别名',
+  `user_email` varchar(100) NOT NULL COMMENT '用户邮箱',
+  `user_phone` varchar(30) NOT NULL COMMENT '用户电话',
+  `user_registered` datetime NOT NULL COMMENT '用户注册时间',
+  `user_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '用户状态',
+  `user_role` tinyint(3) unsigned NOT NULL COMMENT '用户所属角色ID',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `yx_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
