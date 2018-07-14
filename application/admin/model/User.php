4d0052703545415aa11c008ca1238bd8eaf9bea0
gulang <1226740471@qq.com>
2018-07-14 16:50:50 +0800

    user model

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -4,5 +4,22 @@ use think\Model;
 
 class User extends Model
 {
-      
+    
+    
+    
+    public function userList(){
+          // to do
+    } 
+
+    public function addUser(){
+          // to do
+    } 
+
+
+    public function delUser(){
+          // to do
+    } 
+
+
+       
 }
