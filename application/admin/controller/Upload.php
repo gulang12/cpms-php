edc77b525675bcf29ef28b5f5311f35c8523e211
gulang <1226740471@qq.com>
2018-08-06 17:25:38 +0800

    upload

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -1,63 +1,48 @@
 <?php
 namespace app\admin\controller;
 
-use app\common\util\PasswordHash;
 class Upload  extends AdminBase
 {
    
-    public function userList()   
-    {
-    	
-        
-        $users =  model('User')->getUserList();
 
-        $this->assign("users",$users);
 
-        return $this->fetch();
-    }
-    
-    public function getUser() {
-        $userId = input('param.user_id');
+    public function checkFileInfo() {
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('file');
 
-        $user   = model("User")->getUser($userId);
-        
-        return $user;
-    }
-    
-    public function getRoles(){
+        $info = $file->validate(['size'=>125678,'ext'=>'jpg,png,gif'])->check();  //检测上传文件信息，为了节省空间，所以并不会立即上传文件
 
-        $roles  = model('Role')->getSelectRoles();
-
-        return $roles ? json(['code'=>1,'roles'=>$roles,'msg'=>'获取数据成功']) : json(['code'=>0,'roles'=>'','msg'=>'获取数据成功']);
-    }
-    public function addUser(){
-        
-        $input = input();
-
-        $info  =  model('User')->addUser($input);
-       
-        return $info;
-    }
-    
-    public function delUser(){
-        
-        $userId = input('param.user_id');
-        
-        $info   =  model('User')->delUser($userId);
-    
-        return $info;
-        
-    }
-    
-    public function updateUser(){
+        if(!$info) {
            
-        $input  = input();
-         
-        $info   = model('User')->updateUser($input);
-        
-        return $info;
-      
+           return json(['code'=>1,'msg'=>'图片符合规定']);
+        }else{
+
+           return json(['code'=>2,'msg'=>$file->getError()]);
+        }
+
     }
 
+
+    public function upload(){
+
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('file');
+
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        $info = $file->validate(['size'=>15678,'ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'upload');
+
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 jpg
+            echo $info->getExtension();
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getSaveName();
+            // 输出 42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getFilename(); 
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
     
 }
