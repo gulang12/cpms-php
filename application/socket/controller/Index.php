227da2642d1268859d8ae759336c8887103f97ac
gulang12 <1226740471@qq.com>
2019-07-12 19:53:04 +0800

    Update Index.php

--- application/socket/controller/Index.php
+++ application/socket/controller/Index.php
@@ -2,14 +2,19 @@
 namespace app\socket\controller;
 
 use think\Controller;
-
+use think\Db;
 class Index extends Controller   
 {
 
     public function index()
     {
 
-        
+       	
+       	$userInfo = Db::name('user')->where('user_id',session("user_id"))->find();
+
+      
+        $this->assign("userInfo",	$userInfo);
+
         return $this->fetch();
     }
 
